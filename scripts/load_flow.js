b09f493b0568a0a5b101b9fca1992af29818d896
Daniel Gutierrez <cdanielgl@gmail.com>
2023-09-04 14:45:18 -0600

    talent fest

--- scripts/load_flow.js
+++ scripts/load_flow.js
@@ -1,68 +1,68 @@
 const axios = require('axios');
 const chatFlow = require('../test/flow.json');
 
-// eslint-disable-next-line no-undef
 const PORT = process.env.PORT;
 const baseUrl = `http://localhost:${PORT}/api/v1`;
 const openAIApiKey = process.env.OPENAI_API_KEY;
-console.log(openAIApiKey);
 
-// const createCredential = async (name, apiKey) => {
-//   const payload = {
-//     name,
-//     credentialName: 'openAIApi',
-//     plainDataObj: { openAIApiKey: apiKey },
-//   };
-//   const { data } = await axios.post(`${baseUrl}/credentials`, payload);
-//   return data.id;
-// };
+const createCredential = async (name, apiKey) => {
+  const payload = {
+    name,
+    credentialName: 'openAIApi',
+    plainDataObj: { openAIApiKey: apiKey },
+  };
+  const { data } = await axios.post(`${baseUrl}/credentials`, payload);
+  return data.id;
+};
 
-// const createFlow = async (name, credentialId) => {
-//   const nodes = chatFlow.nodes.map((node) => {
-//     if (node.data.type === 'ChatOpenAI') {
-//       node.data.credential = credentialId;
-//     }
-//     return node;
-//   });
+const createFlow = async (name, credentialId) => {
+  const nodes = chatFlow.nodes.map((node) => {
+    if (node.data.type === 'ChatOpenAI') {
+      node.data.credential = credentialId;
+    }
+    return node;
+  });
 
-//   const chatFlowCredential = { ...chatFlow, nodes };
+  const chatFlowCredential = { ...chatFlow, nodes };
 
-//   const payload = {
-//     deployed: false,
-//     flowData: JSON.stringify(chatFlowCredential),
-//     isPublic: false,
-//     name,
-//   };
+  const payload = {
+    deployed: false,
+    flowData: JSON.stringify(chatFlowCredential),
+    isPublic: false,
+    name,
+  };
 
-//   const { data } = await axios.post(`${baseUrl}/chatflows`, payload);
-//   //console.log(data);
-//   return data.id;
-// };
+  const { data } = await axios.post(`${baseUrl}/chatflows`, payload);
+  return data.id;
+};
 
-// const testEndpoint = async (endpoint) => {
-//   console.log(`${baseUrl}/prediction/${endpoint}`);
-//   const response = await axios
-//     .post(
-//       `${baseUrl}/prediction/${endpoint}`,
-//       JSON.stringify({
-//         question: 'jabon de coco',
-//         history: [],
-//         overrideConfig: {},
-//       })
-//     )
-//     .catch((err) => err);
-//   //console.log(response);
-//   return response.status === 200;
-// };
+const testEndpoint = async (endpoint) => {
+  console.log(`${baseUrl}/prediction/${endpoint}`);
+  try {
+    const response = await axios
+      .post(
+        `${baseUrl}/prediction/${endpoint}`,
+        JSON.stringify({
+          question: 'pozole',
+          history: [],
+          overrideConfig: {},
+        })
+      )
+      .catch((err) => err);
+    return response.status === 200;
+  } catch (err) {
+    throw err;
+  }
+};
 
-// const fullFlow = async (flowName) => {
-//   const credentialId = await createCredential(
-//     'credential script',
-//     openAIApiKey
-//   );
-//   const flowId = await createFlow(flowName, credentialId);
-//   console.log(flowId);
-//   return await testEndpoint(flowId);
-// };
+const fullFlow = async (flowName) => {
+  const credentialId = await createCredential(
+    'credential script',
+    openAIApiKey
+  );
+  const flowId = await createFlow(flowName, credentialId);
+  console.log(flowId);
+  return await testEndpoint(flowId);
+};
 
-// fullFlow('chatFlow').then(console.log).catch(console.log);
+fullFlow('chatFlow').then(console.log).catch(console.log);
